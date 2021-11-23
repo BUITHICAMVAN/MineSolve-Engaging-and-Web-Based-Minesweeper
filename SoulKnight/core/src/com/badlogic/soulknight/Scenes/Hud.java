16f94071c58957632574296c4a006bb32e754ca4
Cong Thuan <81848101+Torches0307@users.noreply.github.com>
2021-11-23 09:08:28 +0700

    New files for SoulKnight
    This is a new project file

---
+++ SoulKnight/core/src/com/badlogic/soulknight/Scenes/Hud.java
@@ -0,0 +1,60 @@
+package com.badlogic.soulknight.Scenes;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.soulknight.SoulKnight;
+
+
+public class Hud {
+    public Stage stage;
+    private Viewport viewport;
+
+    private int worldTimer;
+    private float timeCount;
+    private int score;
+
+    Label countdownLabel;
+    Label scoreLabel;
+    Label timeLabel;
+    Label levelLabel;
+    Label worldLabel;
+    Label soulKnightLabel;
+
+    public Hud (SpriteBatch sb){
+        worldTimer = 300;
+        timeCount = 0;
+        score = 0;
+        viewport = new FitViewport(SoulKnight.V_WIDTH, SoulKnight.V_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport, sb);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        timeLabel = new Label("Time", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        levelLabel = new Label("1 -1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        soulKnightLabel = new Label("SoulKnight", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        table.add(soulKnightLabel).expandX().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
+
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(levelLabel).expandX();
+        table.add(countdownLabel).expandX();
+
+        stage.addActor(table);
+    }
+
+}
---
+++ SoulKnight/desktop/src/com/badlogic/soulknight/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.badlogic.soulknight.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.soulknight.SoulKnight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SoulKnight(), config);
+		config.title = "Soul Knight";
+		config.width = 1584;
+		config.height = 891;
+		config.forceExit = false;
+	}
+}
