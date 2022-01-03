01768a6d5073a88cd34afcf34e83f73a84a203a6
BUI CAM VAN <81848643+BUITHICAMVAN@users.noreply.github.com>
2022-01-04 00:37:44 +0700

    Update map and add more tilesets

---
+++ SoulKnight/core/assets/Weapons/Trees&Plants.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Trees&amp;Plants" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="C:/Users/Admin/Desktop/Soul-Knight-Clone-main/SoulKnight/core/assets/Tree&amp;Plants.png" width="128" height="32"/>
+</tileset>
