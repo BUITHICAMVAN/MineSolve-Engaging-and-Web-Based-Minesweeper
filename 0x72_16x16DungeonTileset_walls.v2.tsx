8ce892eae5e75617749eb3b0097c5b0f9bdbc3f3
Van Bui <81848643+BUITHICAMVAN@users.noreply.github.com>
2022-01-05 23:37:49 +0700

    Delete 0x72_16x16DungeonTileset_walls.v2.tsx

--- 0x72_16x16DungeonTileset_walls.v2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="0x72_16x16DungeonTileset_walls.v2" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="0x72_16x16DungeonTileset_walls.v2.png" trans="ff00ff" width="256" height="256"/>
-</tileset>
