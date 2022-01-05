f51cdd66283cf16d86500561983f41f4cee9b069
Van Bui <81848643+BUITHICAMVAN@users.noreply.github.com>
2022-01-05 23:38:06 +0700

    Delete Barriers.tsx

--- Barriers.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Barriers " tilewidth="16" tileheight="16" tilecount="128" columns="32">
- <image source="Barriers.png" width="512" height="64"/>
-</tileset>
